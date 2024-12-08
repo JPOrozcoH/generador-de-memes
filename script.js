0a996bb8d41cf4246580a1da949aaae0b1a51bee
JPOrozcoH <codedude1507@gmail.com>
2024-12-08 12:39:44 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('nuevo-meme').addEventListener('click', function() {
+    var imagen = document.getElementById('imagen');
+    var imagenes = [];
+    for (var i = 1; i <= 113; i++) {
+        imagenes.push('memes/meme (' + i + ').png');
+    }
+    var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+    imagen.src = imagenes[indiceAleatorio];
+    imagen.style.display = 'block';
+});
