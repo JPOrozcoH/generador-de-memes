c36b573fdc0bfd8f34c38d6af771186ba073b16c
JPOrozcoH <codedude1507@gmail.com>
2024-12-16 13:38:53 -0500

    Se agregan nuevos memes + modificación de archivo JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('nuevo-meme').addEventListener('click', function() {
     var imagen = document.getElementById('imagen');
     var imagenes = [];
-    for (var i = 1; i <= 165; i++) {
+    for (var i = 1; i <= 168; i++) {
         imagenes.push('memes/meme (' + i + ').png');
     }
     var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
