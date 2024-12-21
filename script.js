e1a2f7b624afe8563a850ee16b22a655018dc82d
JuanPOrozco <codedude1507@gmail.com>
2024-12-21 11:20:36 -0500

    Agrego un meme adicional + archivo JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('nuevo-meme').addEventListener('click', function() {
     var imagen = document.getElementById('imagen');
     var imagenes = [];
-    for (var i = 1; i <= 208; i++) {
+    for (var i = 1; i <= 209; i++) {
         imagenes.push('memes/meme (' + i + ').png');
     }
     var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
